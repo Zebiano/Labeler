87aa1add6b21ec8b63fd39cfc00befbe353a8737
Zebiano <sebas1@hotmail.de>
2020-03-09 18:14:43 +0100

    Add: Config

--- labeler.js
+++ labeler.js
@@ -3,6 +3,7 @@
 
 // Requires: Packages
 const meow = require('meow')
+const clear = require('clear')
 
 // Requires: Libs
 const inquirer = require('./lib/inquirer')
@@ -26,6 +27,9 @@ DESCRIPTION
     This CLI helps you organize your GitHub labels by storing them in a file called labels.json. You can add new labels through the CLI, with the -n flag. Whenever you create a new repository, run this script with the -du flag to delete the default labels and upload your custom ones!
 
 OPTIONS
+    -c, --config
+        Launch interactive CLI to store data into config. Storing empty strings removes data from config.
+
     -r, --repository
         Specify GitHub repository name. If not specified uses values in config, else ignores config.
 
@@ -41,9 +45,6 @@ OPTIONS
     -d, --deleteAllLabels
         Delete all existing labels in repository.
 
-    -c, --config
-        Launch interactive CLI to store data into config.
-
     -n, --newLabel
         Launch interactive CLI to store new labels in the labels.json file.
 
@@ -51,7 +52,7 @@ OPTIONS
         Upload custom labels.
 
 EXAMPLES
-    Delete all labels from the repo, and upload custom ones stored under labels.json:
+    Delete all labels from the repository and upload custom ones stored under labels.json:
         labeler -dur repositoryName
 `;
 
@@ -90,54 +91,124 @@ const cli = meow(helpText, {
         uploadLabel: {
             alias: 'u',
             type: 'boolean'
-        },
+        }
     }
 })
 
 /* --- Functions --- */
-// Check for flags, and use config if needed
-function checkFlag(key) {
-    if (cli.flags.hasOwnProperty(key)) {
-        return cli.flags[key]
-    } else if (config.hasKey(key)) {
-        return config.getKey(key)
+// Returns flag from arguments, or from config
+function assignFlag(flag) {
+    if (cli.flags.hasOwnProperty(flag)) {
+        return cli.flags[flag]
+    } else if (config.has(flag)) {
+        return config.get(flag)
     } else {
         return null
     }
 }
 
-// Main
+// Deletes all labels from a repo
+async function deleteAllLabels() {
+    // Check flags
+    if (!token && !owner && !repo) {
+        echo.error('Missing arguments.')
+        echo.info('Use -h for help.', true)
+    } else if (!token) {
+        echo.error('You need to specify a token!')
+        echo.tip('Use the -t flag.', true)
+    } else if (!owner) {
+        echo.error('You need to specify an owner!')
+        echo.tip('Use the -o flag.', true)
+    } else if (!repo) {
+        echo.error('You need to specify a repository!')
+        echo.tip('Use the -r flag.', true)
+    }
+
+    // Ask if the user is sure
+    const answer = await inquirer.confirmDeleteAllLabels()
+    if (!answer.deleteAllLabels) echo.warning('Aborted deletion of all labels from ' + repo + '.', true)
+
+    // Variables
+    let arrayPromises = []
+
+    // Get all labels form repo
+    const allLabels = await requests.getLabels(token, owner, repo)
+
+    // Push promises that delete labels to an array
+    for (let i in allLabels.data) {
+        arrayPromises.push(requests.deleteLabel(
+            token,
+            owner,
+            repo,
+            allLabels.data[i].name
+        ))
+    }
+
+    // Run promises (aka delete all labels)
+    Promise.all(arrayPromises).then(values => {
+        console.log(values);
+    });
+
+    // Done
+    echo.success('Finished!', true)
+}
+
+// Opens the interactive config CLI
+async function configCli() {
+    clear() // Clear
+
+    // Display current config saved
+    console.log("Current config:")
+    console.log(config.getAll()) // TODO: Censor the token out (show last 4 characters maybe)
+    console.log()
+
+    // Get config input from user
+    let answer = await inquirer.config()
+
+    // Check input
+    if (answer.hasOwnProperty('token')) {
+        // Token
+        if (answer.token) config.set(answer)
+        else config.remove('token')
+    } else if (answer.hasOwnProperty('owner')) {
+        // Owner
+        if (answer.owner) config.set(answer)
+        else config.remove('owner')
+    } else if (answer.hasOwnProperty('repository')) {
+        // Repository
+        if (answer.repository) config.set(answer)
+        else config.remove('repository')
+    } else {
+        // Exit
+        clear()
+        echo.success('Finished!', true)
+    }
+
+    // Call this function again until user exits
+    await configCli()
+}
+
+/* --- Start --- */
+// Variables
+const token = assignFlag('token')
+const owner = assignFlag('owner')
+const repo = assignFlag('repository')
+
+// Main function
 async function main() {
-    console.log(cli.flags)
+    // console.log(cli.flags)
     // console.log(config.getAll())
 
     // Delete all labels from a repo
-    if (cli.flags.deleteAllLabels) {
-        // Get all labels form repo
-        const allLabels = await requests.getLabels(checkFlag('token'), checkFlag('owner'), checkFlag('repository'))
+    if (cli.flags.deleteAllLabels) await deleteAllLabels()
 
-        // Push promises that delete labels to array 
-        let arrayPromises = []
-        for (let i in allLabels.data) {
-            arrayPromises.push(requests.deleteLabel(
-                checkFlag('token'),
-                checkFlag('owner'),
-                checkFlag('repository'),
-                allLabels.data[i].name
-            ))
-        }
+    // Run the config interactive CLI
+    if (cli.flags.config) await configCli()
 
-        // Run promises (aka delete all labels)
-        Promise.all(arrayPromises).then(values => {
-            console.log(values);
-        });
-    }
-
-    // 
-    if (cli.flags.config) {
-        inquirer.askConfig()
-    }
+    // If nothing happens, I'm assuming the user ran without flags
+    echo.error('Missing arguments.')
+    echo.info('Use -h for help.', true)
 }
 
-// Start main
+// Call main()
 main()
