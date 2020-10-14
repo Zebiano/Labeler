5b520b8faefd5e3d3a0d2639547220d71c2e5321
Zebiano <sebas1@hotmail.de>
2020-10-15 00:59:11 +0100

    Update: Code rework
    Issue: Fix #13
    Add: `lib/helper.js`

--- labeler.js
+++ labeler.js
@@ -1,16 +1,13 @@
 #!/usr/bin/env node
 'use strict'
 
-// Requires: Packages
+// Require: Packages
 const meow = require('meow')
-const clear = require('clear')
-const chalk = require('chalk')
 const updateNotifier = require('update-notifier')
 
-// Requires: Libs
+// Require: Libs
 const inquirer = require('./lib/inquirer')
 const config = require('./lib/configstore')
-const axios = require('./lib/axios')
 const echo = require('./lib/echo')
 const helper = require('./lib/helper')
 
@@ -151,10 +148,10 @@ console.log()
 updateNotifier({ pkg }).notify({ isGlobal: true })
 
 // Variables
-const token = assignFlag('token')
-const owner = assignFlag('owner')
-const repository = assignFlag('repository')
-const host = assignFlag('host')
+const token = helper.assignFlag(cli, 'token')
+const owner = helper.assignFlag(cli, 'owner')
+const repository = helper.assignFlag(cli, 'repository')
+const host = helper.assignFlag(cli, 'host')
 
 // Main function
 async function main() {
@@ -162,16 +159,34 @@ async function main() {
     if (cli.flags.force) echo.warning('Detected -f, ignoring user confirmation.\n')
 
     // Check if flags were called correctly
-    checkFlags()
+    helper.checkFlags(cli)
+
+    // Check for -d or -u
+    if (cli.flags.deleteAllLabels || cli.flags.uploadLabels) helper.echoOwnerRepository(owner, repository)
 
     // Run functions according to flags
-    if (cli.flags.path) labelsPath() // Return labels.json path
-    if (cli.flags.resetLabelsFile) await resetLabelsFile() // Reset labels.json file
-    if (cli.flags.emptyLabelsFile) await emptyLabelsFile() // Delete all labels from labels.json
-    if (cli.flags.deleteAllLabels) await deleteAllLabels() // Delete all labels from repository
-    if (cli.flags.uploadLabels) await uploadLabels() // Upload custom labels to repository
-    if (cli.flags.config) await cliConfig() // Run the interactive config CLI
-    if (cli.flags.newLabel) await cliNewLabel() // Run the interactive "create new label" CLI
+    if (cli.flags.path) helper.labelsPath() // Return labels.json path
+    if (cli.flags.resetLabelsFile) await helper.resetLabelsFile(cli) // Reset labels.json file
+    if (cli.flags.emptyLabelsFile) await helper.emptyLabelsFile(cli) // Delete all labels from labels.json
+    if (cli.flags.deleteAllLabels) await helper.deleteAllLabels(token, owner, host, repository, cli) // Delete all labels from repository
+    if (cli.flags.uploadLabels) await helper.uploadLabels(token, owner, host, repository, cli) // Upload custom labels to repository
+    if (cli.flags.config) await helper.cliConfig() // Run the interactive config CLI
+    // Run the interactive "create new label" CLI
+    if (cli.flags.newLabel) {
+        echo.tip('If you want to edit the file, here\'s the path:')
+        echo.info(config.path('labels'))
+        console.log()
+
+        // Ask if the user wants a fresh file or not
+        if (!cli.flags.force && !cli.flags.emptyLabelsFile) {
+            const answerFresh = await inquirer.choiceFreshNewLabels()
+            if (answerFresh) config.set('labels', { 'labels': [] })
+            console.log()
+        }
+
+        echo.info('Create new labels:')
+        await helper.cliNewLabel(cli)
+    }
     if (cli.input[0] == "secret") echo.rainbow() // :)
 
     // If any of these flags is true, exit (these are the ones that can always be called, no matter what)
@@ -185,260 +200,3 @@ async function main() {
 
 // Call main()
 main()
-
-/* --- Functions --- */
-// Echo the owner and repository
-function echoOwnerRepository() {
-    echo.owner(owner)
-    echo.repository(repository)
-    console.log()
-}
-
-// Returns flag from arguments, or from config
-function assignFlag(flag) {
-    if (cli.flags.hasOwnProperty(flag)) {
-        return cli.flags[flag]
-    } else if (config.has('config', flag)) {
-        return config.get('config', flag)
-    } else {
-        if (flag == 'host') return pkg.labeler.defaultHost
-        else return null
-    }
-}
-
-// Check required flags
-function checkRequiredFlags() {
-    if (!token && !owner && !repository) {
-        echo.error('Missing arguments.')
-        echo.tip('Use -h for help.', true)
-    } else if (!token) {
-        echo.error('You need to specify a token!')
-        echo.tip('Use the -t flag.', true)
-    } else if (!owner) {
-        echo.error('You need to specify an owner!')
-        echo.tip('Use the -o flag.', true)
-    } else if (!repository) {
-        echo.error('You need to specify a repository!')
-        echo.tip('Use the -r flag.', true)
-    }
-}
-
-// Check flags
-function checkFlags() {
-    // console.log(cli.flags)
-    // All flags to copy easily (without cli.flags.force and cli.flags.help)
-    // cli.flags.repository cli.flags.token cli.flags.owner cli.flags.host cli.flags.uploadLabels cli.flags.deleteAllLabels cli.flags.newLabel cli.flags.config cli.flags.emptyLabelsFile cli.flags.resetLabelsFile
-
-    // Check for usage of flags that shouldn't be used together
-    // If (((Flags) && (Flags)) || (cli.flags.config && cli.flags.newLabel) || (cli.flags.emptyLabelsFile && (Flags)))
-    if (((cli.flags.repository || cli.flags.token || cli.flags.owner || cli.flags.host || cli.flags.uploadLabels || cli.flags.deleteAllLabels) && (cli.flags.newLabel || cli.flags.config))
-        || (cli.flags.config && cli.flags.newLabel)
-        || (cli.flags.emptyLabelsFile && (cli.flags.repository || cli.flags.token || cli.flags.owner || cli.flags.host || cli.flags.uploadLabels || cli.flags.deleteAllLabels || cli.flags.config || cli.flags.resetLabelsFile))) {
-        echo.error('Wrong usage.')
-        echo.tip('Use -h for help.', true)
-    }
-}
-
-// Echos the path for labels.json
-function labelsPath() {
-    echo.info('Path for labels.json')
-    echo.info(config.path('labels'))
-    console.log()
-}
-
-// Deletes labels.json and creates it again with default values from labels.json
-async function resetLabelsFile() {
-    // Ask if the user is sure
-    if (!cli.flags.force) {
-        const answer = await inquirer.confirmResetLabels()
-        if (!answer.resetLabels) {
-            console.log()
-            echo.abort('Reset labels.json.', true)
-        }
-    }
-
-    echo.info('Resetting labels.json...')
-    config.resetLabels()
-    echo.success('Done!\n')
-}
-
-// Empties all labels from labels.json
-async function emptyLabelsFile() {
-    // Ask if the user is sure
-    if (!cli.flags.force) {
-        const answer = await inquirer.confirmEmptyLabels()
-        if (!answer.emptyLabels) {
-            console.log()
-            echo.abort('Delete labels from labels.json.', true)
-        }
-    }
-
-    // Empty labels.json
-    echo.info('Emptying labels.json...')
-    config.set('labels', { 'labels': [] })
-    if (cli.flags.newLabel) echo.success('Done.\n')
-    else {
-        echo.success('Done.\n', true)
-    }
-}
-
-// Deletes all labels from a repository
-async function deleteAllLabels() {
-    checkRequiredFlags() // Check required Flags
-    echoOwnerRepository() // Echo repo and owner
-
-    // Ask if the user is sure
-    if (!cli.flags.force) {
-        const answer = await inquirer.confirmDeleteAllLabels()
-        if (!answer.deleteAllLabels) {
-            console.log()
-            echo.abort('Delete labels from ' + repository + '.', true)
-        }
-    }
-    echo.info(chalk.bold('Deleting labels from ' + repository + '...'))
-
-    // Variables
-    let arrayPromises = []
-
-    // Get all labels form repository
-    const allLabels = await axios.getLabels(true, token, owner, host, repository)
-
-    // Push promises (that delete labels) to an array
-    for (let i in allLabels.data) {
-        arrayPromises.push(axios.deleteLabel(
-            false,
-            token,
-            allLabels.data[i]
-        ))
-    }
-
-    // Run promises (aka delete all labels)
-    await Promise.all(arrayPromises)
-
-    // Done
-    if (cli.flags.uploadLabels) echo.success('Done!\n')
-    else {
-        console.log()
-        echo.success('Finished!', true)
-    }
-}
-
-// Upload all labels from labels.json
-async function uploadLabels() {
-    // Check required Flags
-    checkRequiredFlags()
-
-    // Variables
-    labels = config.getAll('labels')
-
-    // Ask if the user is sure
-    if (!cli.flags.force) {
-        const answer = await inquirer.confirmUploadLabels()
-        if (!answer.uploadLabels) {
-            console.log()
-            echo.abort('Upload labels to ' + repository + '.', true)
-        }
-    }
-    echo.info(chalk.bold('Uploading labels to ' + repository + '...'))
-
-    // Variables
-    let arrayPromises = []
-
-    // Push promises (that upload labels) to an array
-    for (let i in labels) {
-        arrayPromises.push(axios.saveLabel(
-            false,
-            token,
-            owner,
-            host,
-            repository,
-            labels[i]
-        ))
-    }
-
-    // Run promises (aka upload all labels)
-    await Promise.all(arrayPromises)
-
-    // Done
-    echo.success('Done!\n')
-    echo.success('Finished!', true)
-}
-
-// Opens the interactive config CLI
-async function cliConfig() {
-    clear() // Clear
-
-    // Display current config
-    echo.info("Current config:")
-    console.log(helper.censorConfig(config.getAll('config')))
-    console.log()
-
-    // Get config input from user
-    let answer = await inquirer.config()
-
-    // Check input
-    if (answer) {
-        if (answer.hasOwnProperty('token')) {
-            // Token
-            if (answer.token) config.set('config', answer)
-            else config.remove('config', 'token')
-        } else if (answer.hasOwnProperty('owner')) {
-            // Owner
-            if (answer.owner) config.set('config', answer)
-            else config.remove('config', 'owner')
-        } else if (answer.hasOwnProperty('repository')) {
-            // Repository
-            if (answer.repository) config.set('config', answer)
-            else config.remove('config', 'repository')
-        } else if (answer.hasOwnProperty('host')) {
-            // Host
-            if (answer.host) config.set('config', answer)
-            else config.remove('config', 'host')
-        } else {
-            // Exit
-            process.exit()
-        }
-    }
-
-    // Call this function again until user exits
-    await cliConfig()
-}
-
-// Opens the interactive "create new label" CLI
-async function cliNewLabel() {
-    echo.tip('If you want to edit the file, here\'s the path:')
-    echo.info(config.path('labels'))
-    console.log()
-
-    // Ask if the user wants a fresh file or not
-    if (!cli.flags.force && !cli.flags.emptyLabelsFile) {
-        const answerFresh = await inquirer.choiceFreshNewLabels()
-        if (answerFresh) config.set('labels', { 'labels': [] })
-        console.log()
-    }
-
-    echo.info('Create new labels:')
-
-    // Variables
-    labels = config.getAll('labels')
-    let dupe = false
-
-    // Get config input from user
-    let answer = await inquirer.newLabel()
-
-    // Check for dupe
-    for (let i in labels) {
-        if (labels[i].name == answer.name) {
-            dupe = true
-            break
-        }
-    }
-    if (!dupe) {
-        labels.push(answer)
-        config.set('labels', { 'labels': labels })
-        echo.success('Saved label! Use Ctrl+C to exit.\n')
-    } else echo.error('Label "' + answer.name + '" already exists! Please choose another name.\n')
-
-    // Call this function again until user exits
-    await cliNewLabel()
-}
